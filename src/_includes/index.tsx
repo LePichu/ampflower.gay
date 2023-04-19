c03814f7c590b5b660f83cec2ebd162008c2b826
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2023-04-19 23:15:04 +0530

    feat: initial commit.

---
+++ src/_includes/index.tsx
@@ -0,0 +1,58 @@
+const URL = `https://ampflower.gay`
+
+interface Props {
+	children: Element
+	meta: {
+		title: string | undefined
+		description: string | undefined
+		image: string | undefined
+		theme: string | undefined
+	}
+	blog: boolean
+}
+
+export default function Layout(props: Props) {
+	return (
+		<>
+			<html lang="en">
+				<head>
+					<meta charSet="UTF-8" />
+					<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0"
+					/>
+
+					<link rel="stylesheet" href="/index.css" />
+					<link rel="icon" href="/assets/icon.png" />
+
+					<title>{props.meta.title ?? "LePichu | Website"}</title>
+
+					<meta
+						name="description"
+						content={props.meta.description ?? "LePichu | Website"}
+					/>
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta
+						property="twitter:image"
+						content={props.meta.image ?? `${URL}/assets/icon.png`}
+					/>
+					<meta
+						property="og:image"
+						content={props.meta.image ?? `${URL}/assets/icon.png`}
+					/>
+					<meta property="og:url" content={URL} />
+					<meta
+						name="theme-color"
+						content={props.meta.theme ?? "#1b3f4b".toUpperCase()}
+					/>
+				</head>
+				<body class="m-0">
+					<main id={props.blog ? "blog-content" : "content"}>
+						{props.children}
+					</main>
+				</body>
+			</html>
+		</>
+	)
+}
